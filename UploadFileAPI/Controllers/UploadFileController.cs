e3276583ecbe66aa99f94cae1e03e7238d9a7c5a
TamNM1992 <106048213+TamNM1992@users.noreply.github.com>
2023-02-24 16:35:52 +0700

    24223

--- UploadFileAPI/Controllers/UploadFileController.cs
+++ UploadFileAPI/Controllers/UploadFileController.cs
@@ -228,6 +228,165 @@ namespace UploadFilesAPI.Controllers
             return Ok(result);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> UploadFilePartnerAsync(List<IFormFile> file, int width, string? Obj_Id, int type, int userId)
+        {
+            const bool AllowLimitSize = true;
+            const bool AllowLimitFileType = true;
+
+            var limitFileSize = 4194304; // allow upload file less 2MB = 2097152
+            var listFileError = new List<FileUploadInfo>();
+            var responseData = new ResponseData();
+            string result = "";
+
+            if (file.Count <= 0)
+            {
+                responseData.status = "ERROR";
+                responseData.message = $"Please, select file to upload.";
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+            var listFileTypeAllow = "jpg|png|gif|xls|xlsx|jpeg|webp|jfif";
+            // check file type upload allow
+            if (AllowLimitFileType)
+            {
+                foreach (var formFile in file)
+                {
+                    var file_ext = Path.GetExtension(formFile.FileName).Replace(".", "");
+                    var isAllow = listFileTypeAllow.Split('|').Any(x => x.ToLower() == file_ext.ToLower());
+                    if (!isAllow)
+                    {
+                        listFileError.Add(new FileUploadInfo()
+                        {
+                            filename = formFile.FileName,
+                            filesize = formFile.Length
+                        });
+                    }
+
+                }
+            }
+
+            if (listFileError.Count > 0)
+            {
+                responseData.status = "ERROR";
+                responseData.data = JsonConvert.SerializeObject(listFileError);
+                responseData.message = $"File type upload only Allow Type: ({listFileTypeAllow}) \r\n {responseData.data}";
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+            // check list file less limit size
+            if (AllowLimitSize)
+            {
+                foreach (var formFile in file)
+                {
+                    if (formFile.Length > limitFileSize)
+                    {
+                        listFileError.Add(new FileUploadInfo()
+                        {
+                            filename = formFile.FileName,
+                            filesize = formFile.Length
+                        });
+                    }
+                }
+
+            }
+
+            var listLinkUploaded = new List<string>();
+            var listFileLenght = new List<int>();
+
+            if (listFileError.Count > 0)
+            {
+                responseData.status = "ERROR";
+                responseData.data = JsonConvert.SerializeObject(listFileError);
+                responseData.message = $"File upload must less 4MB ({responseData.data})";
+                result = JsonConvert.SerializeObject(responseData);
+                return Ok(result);
+            }
+
+            string baseUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+
+            int i = 0;
+            string directoryPath = "";
+            int filelenght = 0;
+            string urlpath = "";
+
+            if (type == 0)
+            {
+                directoryPath = $@"{_webHost.WebRootPath}\partner\hotel\{userId}\{Obj_Id}\";
+            }
+            if (type == 1)
+            {
+                directoryPath = $@"{_webHost.WebRootPath}\partner\room\{userId}\{Obj_Id}\";
+            }
+            if (type == 2)
+            {
+                directoryPath = $@"{_webHost.WebRootPath}\partner\tour\{userId}\{Obj_Id}\";
+            }
+            if (type == 3)
+            {
+                directoryPath = $@"{_webHost.WebRootPath}\partner\news\{userId}\{Obj_Id}\";
+            }
+
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+
+            if (!directory.Exists)
+            {
+                directory.Create();
+            }
+
+            foreach (var formFile in file)
+            {
+                FileHelper.GeneratorFileByDay(FileStype.Log, $"Số lượng file : {file.Count}", "SaveImage");
+                try
+                {
+                    if (formFile.Length > 0)
+                    {
+
+
+                        var time = DateTime.UtcNow;
+                        var timestring = $"{time}";
+
+                        string[] charsToRemove = new string[] { "@", ",", ".", ";", "'", "/", ":", " " };
+                        foreach (var c in charsToRemove)
+                        {
+                            timestring = timestring.Replace(c, string.Empty);
+                        }
+                        var templateUrl = Path.GetExtension(formFile.FileName).Replace(".", $"{timestring}00{i}.");
+
+                        //var templateUrl = file.FileName ;
+                        //string filePath = Path.Combine($"{_webHost.WebRootPath}/uploads/{path}", templateUrl);
+                        string filePath = Path.Combine(directoryPath, templateUrl);
+
+                        string fileName = Path.GetFileName(filePath);
+
+                        using (var stream = System.IO.File.Create(filePath))
+                        {
+
+                            await formFile.CopyToAsync(stream);
+                            filelenght = _uploadServices.ThayDoiKichThuocAnh(filePath, fileName, width, stream);
+                            FileHelper.GeneratorFileByDay(FileStype.Log, $"thành công file {fileName}", "SaveImage");
+                        }
+                        listLinkUploaded.Add(directoryPath + templateUrl);
+                        listFileLenght.Add(filelenght);
+                        i++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    FileHelper.GeneratorFileByDay(FileStype.Log, $"Lỗi rồi : {ex.ToString()}", "SaveImage");
+                }
+            }
+
+            responseData.status = "SUCCESS";
+            responseData.data = JsonConvert.SerializeObject(listLinkUploaded);
+            responseData.size = JsonConvert.SerializeObject(listFileLenght);
+            responseData.message = $"uploaded {file.Count} files successful.";
+            result = JsonConvert.SerializeObject(responseData);
+
+            return Ok(result);
+        }
 
     }
 }
---
+++ UploadFileAPI/Helper/ImageType.cs
@@ -0,0 +1,13 @@
+﻿namespace UploadFileAPI.Helper
+{
+    public class ImageType
+    {
+        public const int hotel = 0;
+        public const int room = 1;
+        public const int tour = 2;
+        public const int news = 3;
+        public const int partneRoom = 5;
+
+
+    }
+}
